2a62eafb11fb6713d1e8e9ea5d761e8a05b88536
Rizwan Ali <rizwan.ali@ibm.com>
2019-01-14 16:05:16 -0600

    Initial design finalized. Implemented on Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,79 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Checkpoint | Login</title>
+
+    <!-- Bootstrap -->
+    <link href="{{ url('assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{url('assets/vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="{{url('assets/vendors/nprogress/nprogress.css')}}" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="{{url('assets/vendors/animate.css/animate.min.css')}}" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="{{url('assets/build/css/custom.min.css')}}" rel="stylesheet">
+  </head>
+
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            @if ($errors->any())
+              <div class="alert alert-danger fade in" role="alert">
+                          {{ $errors->first() }}
+                        </div>
+            @endif
+            @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                  {{ session('status') }}
+              </div>
+            @endif
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+              <h1>Penta A Checkpoint</h1>
+              <div>
+                <input type="email" name="email" id="email" class="form-control" placeholder="E-Mail Address" required autofocus value="{{ old('email') }}" />
+              </div>
+              <div>
+                <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
+              </div>
+              <div>
+                <button class="btn btn-default submit" type="submit">Log in</button>
+                <a class="reset_pass" href="{{ route('password.request') }}">Lost your password?</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+
+                  <p>©2019 All Rights Reserved. Penta A!</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +145,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
